b77d5126bb6ec9f23df7ae584b2f04c2e29efd28
Temillionaire <olayinkatemitayo13@gmail.com>
2026-01-04 22:32:07 +0100

    Change mail port to 587

--- src/config/email.js
+++ src/config/email.js
@@ -2,9 +2,10 @@ const nodemailer = require('nodemailer'); //to install nodemailer
 
 
 const transporter = nodemailer.createTransport({
+    service: 'gmail',
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true, // true for 465, false for other ports
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
